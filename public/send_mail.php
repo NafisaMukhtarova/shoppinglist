8ae166821f03b8b4aad60de133112825a27b2bba
user <user@myhost>
2021-07-23 15:50:29 +0500

    phpmailer

--- public/send_mail.php
+++ public/send_mail.php
@@ -2,9 +2,10 @@
 
 require_once 'bootstrap.php';
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
 $id=1;
-$config = new Config;
-$pdo = $config->Connect_PDO();
 $id= $_GET['list_id'];
 
 //продукты из списка
@@ -52,15 +53,19 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
     var_dump ($_POST);
     $mail = new PHPMailer(true);
 
+    $host = $_ENV['MAIL_HOST'];
+    $username = $_ENV['MAIL_USERNAME'];
+    $pass = $_ENV['MAIL_PASSWORD'];
+
     try {
         //Server settings
         $mail->SMTPDebug = 0;                      //Enable verbose debug output
         $mail->CharSet = "utf-8";
         $mail->isSMTP();                                            //Send using SMTP
-        $mail->Host       = 'smtp.yandex.com';                     //Set the SMTP server to send through
+        $mail->Host       = $host;                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'nafisa@ufa-lanka.com';                     //SMTP username
-        $mail->Password   = '5Ims0T004j6RKSYdD6Ma';                               //SMTP password
+        $mail->Username   = $username;                     //SMTP username
+        $mail->Password   = $pass;                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
         $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
